e2f32c8f6d2ec57a00fb9b1e75248b5dcc7ec5a4
justinbiebergoth <hiirari666@gmail.com>
2022-12-10 18:14:47 +0300

    Initial Commit

---
+++ TrafficLightSui/TrafficLightSuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSuiApp.swift
+//  TrafficLightSui
+//
+//  Created by hiirari on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
